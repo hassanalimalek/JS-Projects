ceb207519ae4a520b60b7d3459db7540b627c4ff
Hassan Ali <hassanalimalek9@gmail.com>
2023-04-27 08:30:12 +0500

    functionality added

---
+++ dice-roll/app.js
@@ -0,0 +1,13 @@
+const rollDiceBtn = document.querySelector('.dice__trigger');
+const diceImg = document.querySelector('.dice__image')
+const diceResultDiv = document.querySelector('.dice__result')
+
+
+rollDiceBtn.addEventListener('click',()=>{
+    diceResultDiv.classList.add('dice__animate')
+    setTimeout(()=>{
+        let random = Math.round(Math.random() * 5) + 1 ;
+        diceImg.src = `./images/dice${random}.png`
+        diceResultDiv.classList.remove('dice__animate')
+    },[1000])
+})
