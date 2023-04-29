98fd614ac535b4c58d6c03a58aeb186a7b3a11de
Hassan Ali <hassanalimalek9@gmail.com>
2023-04-29 21:44:35 +0500

    header navbar added

--- js/tracker.js
+++ js/tracker.js
@@ -1,7 +1,3 @@
-
-import SongsList from './data.json' assert { type: "json" }
-import { PlayList } from "./playlist.js";
-
 export const DurationTracker = (()=>{
 
     let currentDuration = 0;
@@ -11,9 +7,7 @@ export const DurationTracker = (()=>{
     const setState = (obj)=>{
         currentDuration = obj.currentDuration;
         totalDuration = obj.totalDuration;
-        console.log(Math.round(obj.currentDuration/obj.totalDuration * 100))
         width = Math.round(obj.currentDuration/obj.totalDuration * 100);
-        console.log("obj re",obj)
         render()
     }
   
@@ -26,12 +20,7 @@ export const DurationTracker = (()=>{
         listeners();
     }
     const render = ()=>{
-        console.log("render @@@@")
         progressInnerEl.style.width = `${width}%`;
-        // songNameMainEl.innerText = currentSongObj.name;
-        // songImageEl.src= currentSongObj.coverArt;
-        // totalSongsEl.innerText = PlayList.totalSongs + ' Songs';
-        // playBtnMainEl.innerText = currentSong.paused ? 'Play' : 'Pause'
     }
     
     return {
